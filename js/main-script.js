39d86c485107bd116ece30466f3abd6a2b67faf2
Leonid <leomalinovsky@gmail.com>
2017-11-23 22:19:09 +0200

    Numb counter + icons X + map block

--- js/main-script.js
+++ js/main-script.js
@@ -5,3 +5,42 @@ $('div.search').on('click', '#searchbutton', () => {
 $('.close-btn').on('click', () => {
     $('#searchplace').css('display', 'none');
 });
+
+const animateNumb = () => {
+		$('#animNumb3')
+		.prop('number', 0)
+		.animateNumber(
+		{
+		  number: 3
+		},
+		5000
+		);
+
+		$('#animNumb60')
+		.prop('number', 0)
+		.animateNumber(
+		{
+		  number: 60
+		},
+		5000
+		);
+
+		$('#animNumb150')
+		.prop('number', 0)
+		.animateNumber(
+		{
+		  number: 150
+		},
+		5000
+		);
+};
+
+$(window).scroll(function(){
+	const st = $(this).scrollTop();
+	console.log(st);
+	if (st >= 270 && st <= 300){
+		animateNumb();
+	}
+});
+
+
